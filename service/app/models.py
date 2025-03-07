f6f081a4bd8c975f3db13db255489f2d9ff8ee8b
egor-morozik <egor.mrz04@gmail.com>
2025-03-07 23:29:28 +0300

    -Create models -Make migrations

--- service/app/models.py
+++ service/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class UserManager(models.Manager):
+    def create_user(self, email, password):
+        user = self.model(email=email, password=password)
+        user.save(using=self._db)
+        return user
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    objects = UserManager()
+    
