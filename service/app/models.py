7ee1ef12fc32b40d5dc84fa65a0313f0db7fb991
egor-morozik <egor.mrz04@gmail.com>
2025-03-08 12:26:49 +0300

    -Create Task model -Make migtaions

--- service/app/models.py
+++ service/app/models.py
@@ -11,4 +11,25 @@ class User(models.Model):
     password = models.CharField(max_length=128)
 
     objects = UserManager()
-    
+
+    def check_password(self, raw_password):
+        return self.password == raw_password
+
+    def __str__(self):
+        return self.email
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('TODO', 'To Do'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('DONE', 'Done'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='TODO')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
